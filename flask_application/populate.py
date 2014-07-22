df9e4b078f5df86d2491c8d01c01dca2c281baeb
Allan See <asee@ewise.com>
2014-07-22 14:54:13 +0100

    Added test todo data.

--- flask_application/populate.py
+++ flask_application/populate.py
@@ -1,5 +1,7 @@
-from flask_application import user_datastore
+import datetime
 
+from flask_application import app, user_datastore
+from flask_application.models import User, Todo
 
 def create_roles():
     for role in ('admin', 'author'):
@@ -10,12 +12,19 @@ def create_roles():
 def create_users():
     for u in (('matt', 'matt@lp.com', 'password', ['admin'], True),
               ('jill', 'jill@lp.com', 'password', ['author'], True),
-              ('tiya', 'tiya@lp.com', 'password', [], False)):
+              ('tiya', 'tiya@lp.com', 'password', [], False)): 
         user_datastore.create_user(email=u[1], password=u[2],
                                    roles=u[3], active=u[4])
         user_datastore.commit()
 
+def create_todos():
+    for u in User.query.all():
+        for i in xrange(1, 51):
+            todo = Todo(item="Item Todo for %s #%0d" % (u.email, i), date=datetime.datetime.now(), owner=u.id)
+            app.db.session.add(todo)
+    app.db.session.commit()
 
 def populate_data():
     create_roles()
     create_users()
+    create_todos()
