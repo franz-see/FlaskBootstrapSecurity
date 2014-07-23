8bad452dd2f1bc7dc7707e1f184bdc1f6f077dec
Allan See <asee@ewise.com>
2014-07-23 10:10:26 +0100

    Merged todoModalCtrl and todoAddCtrl into todoViewAddModalCtrl

--- static/js/todo/todoViewAddModalCtrl.js
+++ static/js/todo/todoViewAddModalCtrl.js
@@ -1,9 +1,23 @@
 app.controller('TodoModalCtrl', ['$scope', '$modal', function ($scope, $modal) {
+    $scope.format = app.dateFormat;
     $scope.openModal = function (size) {
 
         var modalInstance = $modal.open({
             templateUrl: 'todoAddCtrl.html',
             controller: function ($scope, $modalInstance) {
+                $scope.dt = new Date();
+                $scope.openDatePicker = function ($event) {
+                    $event.preventDefault();
+                    $event.stopPropagation();
+
+                    $scope.opened = true;
+                };
+
+                $scope.dateOptions = {
+                    formatYear: 'yy',
+                    startingDay: 1
+                };
+
                 $scope.ok = function () {
                     $modalInstance.close();
                 };
