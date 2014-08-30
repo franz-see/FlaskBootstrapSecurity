48981574669d8a82555d16036803ffdfa8ea0d78
Franz See <franz.see@gmail.com>
2014-08-30 16:33:54 +0800

    Splitted up test for TestGet into several tests.

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -11,12 +11,7 @@ class TestDatetimeToMillis(BaseTestCase):
         expected_millis = 1409378747880 
         self.assertEquals(actual_millis, expected_millis)
 
-class TestGet(BaseTestCase):
-
-    def test_should_get_value_from_dict(self):
-        actual_value = utils.get({'dummy key':'dummy value'}, 'dummy key')
-        self.assertEquals(actual_value, 'dummy value') 
-
+class _Given_ValueIsNone_When_Get:
     def test_given_value_is_none_then_return_default_value(self):
         actual_value = utils.get({'key':None}, 'key', value_if_none='default value')
         self.assertEquals(actual_value, 'default value')
@@ -24,6 +19,15 @@ class TestGet(BaseTestCase):
     def test_key_does_not_exist_then_return_default_value(self):
         actual_value = utils.get({}, 'key', value_if_none='default value')
         self.assertEquals(actual_value, 'default value')
+
+class _Given_MaxValue_WhenGet:
+    def test_given_value_is_none(self):
+        actual_value = utils.get({'key':None}, 'key', max_value=100)
+        self.assertEquals(actual_value, 100)
+
+    def test_given_key_does_not_exist(self):
+        actual_value = utils.get({}, 'key', max_value=100)
+        self.assertEquals(actual_value, 100)
 
     def test_given_value_is_above_max_value_then_return_max_value_only(self):
         actual_value = utils.get({'key':101}, 'key', max_value=100)
@@ -36,4 +40,10 @@ class TestGet(BaseTestCase):
     def test_given_value_is_equal_to_max_value_then_return_value(self):
         actual_value = utils.get({'key':100}, 'key', max_value=100)
         self.assertEquals(actual_value, 100)
+
+class TestGet(BaseTestCase, _Given_ValueIsNone_When_Get, _Given_MaxValue_WhenGet):
+
+    def test_should_get_value_from_dict(self):
+        actual_value = utils.get({'dummy key':'dummy value'}, 'dummy key')
+        self.assertEquals(actual_value, 'dummy value') 
 
